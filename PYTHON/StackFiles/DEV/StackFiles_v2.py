75e175ff24de0c8876328985258fe5d029695fdc
AaronCHH <aaronhsu219@gmail.com>
2017-02-06 13:20:14 +0800

    update

---
+++ PYTHON/StackFiles/DEV/StackFiles_v2.py
@@ -0,0 +1,24 @@
+import glob, sys, os
+# obtain files
+fileType = sys.argv[1]
+files = glob.glob(fileType)
+
+fout = open('combine.md','at',encoding='utf-8')
+
+for file in files:
+
+    print(file)
+
+    fin = open(file,'rt',encoding='utf-8')
+
+    line = fin.readline()
+
+    while line:
+        fout.write(line)
+        line = fin.readline()
+
+    fin.close()
+
+
+
+fout.close()
---
+++ PYTHON/StackFiles/DEV/stackf.py
@@ -0,0 +1,23 @@
+import glob, sys, os
+# obtain files
+fileType = sys.argv[1]
+files = glob.glob(fileType)
+
+fout = open('stack.md','at',encoding='utf-8')
+
+for file in files:
+
+    print(file)
+
+    fin = open(file,'rt',encoding='utf-8')
+
+    line = fin.readline()
+
+    fout.write(file + '\n')
+    while line:
+        fout.write(line)
+        line = fin.readline()
+
+    fin.close()
+
+fout.close()
